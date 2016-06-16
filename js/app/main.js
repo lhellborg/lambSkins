bf71a488a0ec68d52a95eec4a03f5b5909cd1831
Linda Hellborg <linda.hellborg@gmail.com>
2016-06-16 13:37:20 +0200

    delete lamb skin number in text

--- js/app/main.js
+++ js/app/main.js
@@ -21,9 +21,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 	function appendLambImg(nr) {
-		var lambImg = ('<div class="col-xs-6 col-sm-4 threePic">' +
-							'<img class="img-responsive" src="images/lamm'+nr+'-small.jpg"  alt="lamm'+nr+'" data-toggle="modal" data-target="#lamm'+nr+'">' +
-							'<paper-button class="submitBtn" raised data-toggle="modal" data-target="#lamm'+nr+'">Boka skinn nr '+nr+'</paper-button>' +
+		var lambImg = ('<div class="col-xs-6 col-sm-4 col-md-3 threePic">' +
+							'<img class="img-responsive lambImgs" src="images/lamm'+nr+'-small.jpg"  alt="lamm skinn" data-toggle="modal" data-target="#lamm'+nr+'">' +
+							'<paper-button class="bokBtn" raised data-toggle="modal" data-target="#lamm'+nr+'">Till bokning</paper-button>' +
 							'<hr>' +
 						'</div>')
 		$(".section").append(lambImg);
@@ -36,23 +36,24 @@ document.addEventListener('DOMContentLoaded', function () {
 		var lambModals = ('<div class="modal fade" id="lamm'+nr+'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel'+nr+'" aria-hidden="true">' +
 			  	'<div class="modal-dialog">'+
 			   		 '<div class="modal-content">'+
-			      		'<div class="modal-header">'+
-			        		'<h4 class="modal-title" id="myModalLabel'+nr+'">Lammskinn '+nr+'</h4>'+
-			      		'</div>'+
+			      		// '<div class="modal-header">'+
+			        // 		'<h4 class="modal-title" id="myModalLabel'+nr+'">Lammskinn '+nr+'</h4>'+
+			      		// '</div>'+
 			      		'<div class="modal-body">'+
-			        		'<img class="img-responsive" src="images/lamm'+nr+'-large_x1.jpg" alt="lamm '+nr+'">'+
-			        		'<p>Jag reserverar skinn nr '+nr+'</p>'+
+			        		'<img class="img-responsive" src="images/lamm'+nr+'-medium.jpg" alt="lamm '+nr+'">'+
+			        		'<p>Jag vill reservera detta skinn</p>'+
 			      		'</div>'+
 			      		'<form is="iron-form" id="form">' +
-							  '<paper-input type="text" label="Mitt namn" required auto-validate autofocus autocomplete="on" error-message="Fyll i ditt namn om du vill reservera" id="reserveratNamn'+nr+'" class="accountRequired"></paper-input>' +
+							  '<paper-input type="text" label="Mitt namn" required auto-validate autocomplete="on" error-message="Fyll i ditt namn om du vill reservera" id="reserveratNamn'+nr+'" class="accountRequired"></paper-input>' +
 							  '<gold-phone-input label="_Mitt telefon nr" country-code="46" phone-number-pattern="" id="reserveratTel'+nr+'" class="accountRequired"></gold-phone-input>' +
 							  '<gold-email-input label="Min email adress" auto-validate id="reserveratMail'+nr+'" class="accountRequired"></gold-email-input>' +
 							  '<paper-button type="submit" class="submitBtn" raised onclick="submitForm('+nr+')">Reservera</paper-button>' +
+							  '<paper-button class="closeBtn" data-dismiss="modal" raised >Stäng</paper-button>'+
 							'</form><br>'+
 							'<p id="storageValue'+nr+'"></p>'+
-			      		'<div class="modal-footer">'+
-			      		'<paper-button data-dismiss="modal" raised >Close</paper-button>'+
-			      		'</div>'+
+			      		// '<div class="modal-footer">'+
+			      		// '<paper-button data-dismiss="modal" raised >Stäng</paper-button>'+
+			      		// '</div>'+
 			    	'</div>'+
 			  	'</div>'+
 			'</div>')
