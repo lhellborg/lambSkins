674ca8cbb46cef2373dceb904c3446eeae2e9fb9
Linda Hellborg <linda.hellborg@gmail.com>
2016-06-16 09:58:42 +0200

    add firebase check on submit form

--- js/app/firebase.js
+++ js/app/firebase.js
@@ -1,64 +1,74 @@
-var myFirebaseRef = new Firebase("https://lambskin.firebaseio.com/");
 
-myFirebaseRef.set({
-	lamb1: {
-		name: "linda",
-		tel: "342423",
-		email: "kalle@sdas"
+
+	var myFirebaseRef = new Firebase("https://lambskin.firebaseio.com/");
+
+	function submitForm(nr) {
+
+		myFirebaseRef.on("value", function(snapshot) {
+		 	console.log(snapshot.val());
+
+			var storVal = "storageValue" + nr;
+			document.getElementById(storVal).innerHTML = "";
+			resName = "reserveratNamn" + nr;
+			resTel = "reserveratTel" + nr;
+			resMail = "reserveratMail" + nr;
+			var name = document.getElementById(resName).value;
+			var tel = document.getElementById(resTel).value;
+			var mail = document.getElementById(resMail).value;
+
+			var reservedSkin = skinReservedTest(nr)
+
+			var valid = validateInput(name, tel, mail); //check all the required fields
+
+		    if (valid) {
+		    	if (reservedSkin) {
+		    		document.getElementById(storVal).innerHTML = "Sorry, det här skinnet är redan reserverat";
+		    	} else {
+				var firebaseRefChild = "lamb" + nr;
+				var objectToInsert = {};
+				objectToInsert[firebaseRefChild] = {
+					nr: nr,
+					name: name,
+					tel: tel,
+					email: mail
+				};
+				myFirebaseRef.update(objectToInsert);
+				document.getElementById(storVal).innerHTML = "Bra val! Vi har reserverat det här till dig!";
+				}
+			}
+			else {
+				document.getElementById(storVal).innerHTML= "Var snäll och fyll i alla fälten"; //when some required fields are missing
+			}
+
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
+		  alert("Kunde tyvärr inte komma åt databasen, försök igen :-)")
+		});
+
+
+
 	}
-})
 
+	function skinReservedTest(nr) {
+		var skinNr = "lamb" + nr+ "/name";
+		var reservedSkin = false;
+		myFirebaseRef.child(skinNr).on("value", function(snapshot) {
+	  		reservedSkin = snapshot.val();
+		});
+		return reservedSkin;
+	}
 
-function submitForm(nr) {
-	var storVal = "storageValue" + nr;
-	document.getElementById(storVal).innerHTML = "";
-	resName = "reserveratNamn" + nr;
-	resTel = "reserveratTel" + nr;
-	resMail = "reserveratMail" + nr;
-	var name = document.getElementById(resName).value;
-	var tel = document.getElementById(resTel).value;
-	var mail = document.getElementById(resMail).value;
+	// Since a required field that has never been "touched" will not show any error message, each input that is required has a class "accountRequired" that can be checked to see that the value is not empty.
+	function validateInput(name, tel, mail) {
+		var isValid = true;
 
-	var skinNr = "lamb" + nr+ "/name";
-	var reservedSkin = false;
-	myFirebaseRef.child(skinNr).on("value", function(snapshot) {
-  		reservedSkin = snapshot.val();
-	});
-
-	var valid = validateInput(name, tel, mail); //check all the required fields
-
-    if (valid) {
-    	if (reservedSkin) {
-    		document.getElementById(storVal).innerHTML = "Sorry, det här skinnet är redan reserverat";
-    	} else {
-		var firebaseRefChild = "lamb" + nr;
-		var objectToInsert = {};
-		objectToInsert[firebaseRefChild] = {
-				name: name,
-				tel: tel,
-				email: mail
-			};
-		myFirebaseRef.update(objectToInsert);
-		document.getElementById(storVal).innerHTML = "Bra val! Vi har reserverat det här till dig!";
+		if (name === "") {
+			return isValid = false;
+		} else if (tel === "") {
+			return isValid = false;
+		} else if (mail === "") {
+			return isValid = false;
 		}
-	}
-	else {
-		document.getElementById(storVal).innerHTML= "Var snäll och fyll i alla fälten"; //when some required fields are missing
-	}
-
-}
-
-// Since a required field that has never been "touched" will not show any error message, each input that is required has a class "accountRequired" that can be checked to see that the value is not empty.
-function validateInput(name, tel, mail) {
-	var isValid = true;
-
-	if (name === "") {
-		return isValid = false;
-	} else if (tel === "") {
-		return isValid = false;
-	} else if (mail === "") {
-		return isValid = false;
-	}
-return isValid;
-};
+		return isValid;
+	};
 
--- js/app/main.js
+++ js/app/main.js
@@ -27,7 +27,7 @@ function appendModals(nr) {
 		        		'<p>Jag reserverar skinn nr '+nr+'</p>'+
 		      		'</div>'+
 		      		'<form is="iron-form" id="form">' +
-						  '<paper-input type="text" label="Mitt namn" required auto-validate autocomplete="on" error-message="Fyll i ditt namn om du vill reservera" id="reserveratNamn'+nr+'" class="accountRequired"></paper-input>' +
+						  '<paper-input type="text" label="Mitt namn" required auto-validate autofocus autocomplete="on" error-message="Fyll i ditt namn om du vill reservera" id="reserveratNamn'+nr+'" class="accountRequired"></paper-input>' +
 						  '<gold-phone-input label="_Mitt telefon nr" country-code="46" phone-number-pattern="" id="reserveratTel'+nr+'" class="accountRequired"></gold-phone-input>' +
 						  '<gold-email-input label="Min email adress" auto-validate id="reserveratMail'+nr+'" class="accountRequired"></gold-email-input>' +
 						  '<paper-button type="submit" class="submitBtn" raised onclick="submitForm('+nr+')">Reservera</paper-button>' +
