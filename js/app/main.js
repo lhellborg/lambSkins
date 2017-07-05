a7fb8aae94acb820b8f78b10b32698309be1f15d
Mattias Hellborg Arthursson <mattias@261consulting.com>
2017-07-05 23:04:05 +0200

    Upgraded polymer components

--- js/app/main.js
+++ js/app/main.js
@@ -94,7 +94,8 @@ $(document).ready(function () {
 			      		'</div>'+
 			      		'<form is="iron-form" id="form">' +
 							  '<paper-input type="text" label="Mitt namn" required auto-validate autocomplete="on" autofocus error-message="Fyll i ditt namn om du vill reservera" id="reserveratNamn'+nr+'" class="accountRequired"></paper-input>' +
-							  '<gold-email-input label="Min emailadress" auto-validate id="reserveratMail'+nr+'" class="accountRequired"></gold-email-input>' +
+							  '<paper-input type="text" label="Mitt namn" required auto-validate autocomplete="on" autofocus error-message="Fyll i ditt namn om du vill reservera" id="reserveratMail'+nr+'" class="accountRequired"></paper-input>' +
+							 
 							  '<paper-button type="submit" class="submitBtn" id="reserveBtn'+nr+'" raised onclick="submitForm('+nr+')">Reservera</paper-button>' +
 							  '<paper-button class="closeBtn" data-dismiss="modal" raised >Stäng</paper-button>'+
 						'</form><br>'+
